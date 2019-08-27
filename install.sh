46022c1d77bdb96754eed53f406569e7cdf1a668
jmechnich <joerg.mechnich@gmail.com>
2019-08-27 16:42:34 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+PYTHON=python3
+
 PREFIX=/usr/local
 #PREFIX=/usr
 FILES=files.txt
 
+if ! $PYTHON -c 'import daemon' 2>/dev/null; then
+    echo "Installing ${PYTHON}-daemon"
+    sudo apt-get install ${PYTHON}-daemon
+fi
+
 echo "Installing to $PREFIX, keeping list of files in $FILES"
 echo
 
-sudo python setup.py install --prefix "$PREFIX" --record "$FILES"
+sudo "$PYTHON" setup.py install --prefix "$PREFIX" --record "$FILES"
 
 echo
 echo "Uninstall with 'cat $FILES | sudo xargs rm -rf'"
